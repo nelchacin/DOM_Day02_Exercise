611763d99e62e8c2042e4f3cd71f844d7254598d
nelchacin <nelchacindev@gmail.com>
2022-11-15 21:31:23 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+// const inputOne = document.querySelector(".inputOne")
+// const buttonOne = document.querySelector(".buttonOne")
+// const theLength = document.querySelector(".theLenght")
+// const resetButton = document.querySelector(".resetButton")
+
+// buttonOne.onclick = () => {
+//     const inputValue = inputOne.value.length
+//     document.querySelector(".theLenght").textContent = inputValue
+// }
+
+// resetButton.onclick = () => {
+//     inputOne.value = ""
+//     theLength.textContent = ""
+
+// }
+
+
+
+//---------------EJERCICIO 1---------------------
+const inputName = document.querySelector("#nombreCliente")
+const inputMadera = document.querySelector("#materialMadera")
+const inputHierro = document.querySelector("#materialHierro")
+const materialSelection = document.querySelector("#materialSeleccion")
+
+
+
+
